875be683520c286bdc3837b7a11a0d328efb63d8
Anand <anand261950@gmail.com>
2024-08-26 14:43:49 +0530

    changed cors

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const corsOptions = {
+  origin: "https://student-teacher-booking.onrender.com"
+}
 const app = express();
 const port = 5000;
 
+
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 connectDB();
